7ed8ea6a57db9fe1a70c51e6227e1b0a81611f5a
Maxim Popov <amodelkit@gmail.com>
2023-06-01 15:38:26 +0400

    footsteps + demo music added

--- AGJ/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ AGJ/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,6 +14,7 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID FOOTSTEPS = 2385628198U;
+        static const AkUniqueID INTERACTIVEMUSIC = 2279279248U;
     } // namespace EVENTS
 
     namespace BANKS
